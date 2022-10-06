20aed4b3d6e2c217e45dfe8d574ee219f15d1dc8
AngelFMC <612110393@utsalamanca.edu.mx>
2022-10-05 20:35:59 -0500

    Tarea compañia

--- js/app.js
+++ js/app.js
@@ -15,6 +15,10 @@ const pintarCards = () =>{
       templateCard.querySelectorAll('h7')[1].textContent = user.phone
       templateCard.querySelectorAll('h7')[2].textContent = user.name
       templateCard.querySelectorAll('h7')[3].textContent = user.website
+      templateCard.querySelectorAll('h7')[4].textContent = user.company.name
+      templateCard.querySelectorAll('h7')[5].textContent = user.address.street
+      templateCard.querySelectorAll('h7')[6].textContent = user.address.city
+      templateCard.querySelectorAll('h7')[7].textContent = user.address.zipcode
 
       templateCard.querySelector('.btn-primary').dataset.id = user.id
 
